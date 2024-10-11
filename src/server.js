9b130f521d30ddfe39905145001d56147148896f
quantumbyte <muhammaduzairasif36@gmail.com>
2024-10-11 23:22:28 +0500

    set true proxy

--- src/server.js
+++ src/server.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.set("trust proxy", true);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
